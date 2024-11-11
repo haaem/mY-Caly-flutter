c163cc50c8e49ba2f8ebbcc3473b24a87c860f24
haaem <hyemin17@yonsei.ac.kr>
2024-11-12 00:35:24 +0900

    Feat: 로그인 페이지 추가 & 캘린더 날짜 수정

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -3,6 +3,6 @@ import 'package:my_caly_flutter/pages/login/login_page.dart';
 import 'package:my_caly_flutter/pages/calendar/calendar_page.dart';
 
 final List<GetPage> routes = [
-  GetPage(name: '/login', page: () => const LoginPage(), transition: Transition.zoom),
+  GetPage(name: '/login', page: () => LoginPage(), transition: Transition.zoom),
   GetPage(name: '/calendar', page: () => const CalendarPage(), transition: Transition.fade),
 ];
