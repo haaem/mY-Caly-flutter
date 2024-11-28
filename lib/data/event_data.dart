5569e37ce8c94c44bd8593b2e86331de8630e92e
haaem <hyemin17@yonsei.ac.kr>
2024-11-28 19:02:42 +0900

    Feat: 캘린더 데이터 서버 연동

--- lib/data/event_data.dart
+++ lib/data/event_data.dart
@@ -1,16 +1,86 @@
-import 'dart:convert';
+import 'package:dio/dio.dart';
 import 'package:my_caly_flutter/pages/calendar/event.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class EventData {
-  // JSON 데이터로부터 Event 목록을 생성
-  static List<Event> fromJsonList(String jsonString) {
-    final List<dynamic> jsonList = json.decode(jsonString);
+  static List<Event> fromJsonList(List<dynamic> jsonList) {
     return jsonList.map((json) => Event.fromJson(json)).toList();
   }
 
-  // Event 목록을 JSON 문자열로 변환
-  static String toJsonList(List<Event> events) {
-    final List<Map<String, dynamic>> jsonList = events.map((event) => event.toJson()).toList();
-    return json.encode(jsonList);
+  Future<Map<DateTime, List<Event>>> fetchEventsFromServer() async {
+    Map<DateTime, List<Event>> eventsByDate = {};
+    final dio = Dio();
+
+    // major, tags 가져오기
+    List<String>? tags;
+    String? major;
+    await SharedPreferences.getInstance().then((prefs) {
+      tags = prefs.getStringList('selectedTags');
+      major = prefs.getString('selectedMajor');
+    });
+
+    // tags가 같을 때
+    for (int i = 0; i < tags!.length; i++) {
+      final response = await dio.request(
+        'http://my-caly-cralwer.duckdns.org/api/v1/query/posts/',
+        options: Options(method: 'GET'),
+        queryParameters: {'tags': tags![i]},
+      );
+
+      if (response.statusCode == 200) {
+        _processEvents(response.data, eventsByDate);
+      } else {
+        throw Exception('Failed to load events');
+      }
+    }
+
+    // common 내용
+    final responseCommon = await dio.request(
+      'http://my-caly-cralwer.duckdns.org/api/v1/query/posts/',
+      options: Options(method: 'GET'),
+      queryParameters: {'college': 'common'},
+    );
+
+    if (responseCommon.statusCode == 200) {
+      _processEvents(responseCommon.data, eventsByDate);
+    } else {
+      throw Exception('Failed to load events');
+    }
+
+    // major 내용
+    final responseMajor = await dio.request(
+      'http://my-caly-cralwer.duckdns.org/api/v1/query/posts/',
+      options: Options(method: 'GET'),
+      queryParameters: {'college': major},
+    );
+
+    if (responseMajor.statusCode == 200) {
+      _processEvents(responseMajor.data, eventsByDate);
+    } else {
+      throw Exception('Failed to load events');
+    }
+
+    return eventsByDate;
+  }
+
+  void _processEvents(List<dynamic> eventData, Map<DateTime, List<Event>> eventsByDate) {
+    List<Event> events = fromJsonList(eventData);
+
+    for (var event in events) {
+      DateTime? eventDate = event.endDate;
+      if (eventDate == null) {
+        continue;
+      }
+
+      DateTime date = DateTime(eventDate.year, eventDate.month, eventDate.day);
+
+      if (eventsByDate[date] == null) {
+        eventsByDate[date] = [];
+      }
+
+      if (!eventsByDate[date]!.any((existingEvent) => existingEvent.id == event.id)) {
+        eventsByDate[date]!.add(event);
+      }
+    }
   }
 }
