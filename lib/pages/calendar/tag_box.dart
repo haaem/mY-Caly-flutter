71d24fc699c7ae44679276b51f5a421a555d1212
haaem <hyemin17@yonsei.ac.kr>
2024-12-03 20:51:04 +0900

    Design: drawer 디자인 추가

--- lib/pages/calendar/tag_box.dart
+++ lib/pages/calendar/tag_box.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_caly_flutter/config/color.dart';
 import 'package:my_caly_flutter/config/text/body_text.dart';
 
 class TagBox extends StatelessWidget {
@@ -8,15 +7,16 @@ class TagBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 150,
-      height: 35,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        color: Color(0xffE9ECEF)
+    return IntrinsicWidth(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 7),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: const Color(0xffF4F4F4)
+        ),
+        child: BodyText(text: tag, size: 15, weight: FontWeight.normal,),
       ),
-      child: BodyText(text: tag, size: 15, weight: FontWeight.normal,),
     );
   }
 }
