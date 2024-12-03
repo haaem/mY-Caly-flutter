4de5e97eb6359d854123b983f9b5dc59c745c9d0
haaem <serrysallylee@gmail.com>
2024-12-03 20:51:04 +0900

    Design: drawer 디자인 추가

--- lib/pages/calendar/drawer.dart
+++ lib/pages/calendar/drawer.dart
@@ -4,6 +4,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:my_caly_flutter/config/color.dart';
 import 'package:my_caly_flutter/config/text/body_text.dart';
 import 'package:my_caly_flutter/config/text/title_text.dart';
+import 'package:my_caly_flutter/pages/calendar/tag_box.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:get/get.dart';
 
@@ -63,75 +64,111 @@ class _ProfileDrawerState extends State<ProfileDrawer> {
 
   @override
   Widget build(BuildContext context) {
-    // var media = MediaQuery.of(context).size;
-    // var width = media.width;
+    var media = MediaQuery.of(context).size;
+    var width = media.width;
 
-    return Drawer(
-      child: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(40),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // id
-                _makeString(_getId(), primaryBlack, 35),
-                const SizedBox(height: 10,),
-                // major and tags
-                _makeString(_getMajor(), Colors.black45, 18),
-                const SizedBox(height: 5,),
-                FutureBuilder<List<String>>(
-                  future: _getTags(),
-                  builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const CircularProgressIndicator();
-                    } else if (snapshot.hasError) {
-                      return Text('Error: ${snapshot.error}');
-                    } else if (snapshot.hasData) {
-                      return Text(
-                        snapshot.data!.join(', '),
-                        style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w300,
-                          color: Colors.black45
-                        )
-                      );
-                    } else {
-                      return const Text('No data available');
-                    }
-                  },
-                ),
-                const SizedBox(height: 30,),
-                // check major 버튼
-                GestureDetector(
-                  onTap: () {
-                    Get.toNamed('/check_major');
-                  },
-                  child: Container(
-                    height: 30,
-                    width: 60,
-                    color: primaryBlue,
-                    child: Center(
-                      child: BodyText(text: 'change', color: Colors.white,),
+    return SizedBox(
+      width: width*0.8,
+      child: Drawer(
+        child: Stack(
+          children: [
+            SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // id
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(30,30,30,20),
+                      child: _makeString(_getId(), primaryBlack, 32),
                     ),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    _logout();
-                  },
-                  child: Container(
-                    height: 30,
-                    width: 60,
-                    color: primaryBlue,
-                    child: Center(
-                      child: BodyText(text: 'logout', color: Colors.white,),
+                    Container(
+                      height: 1,
+                      color: primaryGrey,
                     ),
-                  ),
-                )
-              ],
+                    // major and tags
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(25, 25, 25, 0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const BodyText(text: '연세대학교', size: 20, color: primaryBlack,),
+                          const SizedBox(height: 8,),
+                          _makeString(_getMajor(), primaryBlack, 20),
+                          const SizedBox(height: 25,),
+                          FutureBuilder<List<String>>(
+                            future: _getTags(),
+                            builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+                              // 로딩 중일 때
+                              if (snapshot.connectionState == ConnectionState.waiting) {
+                                return const Center(child: CircularProgressIndicator());
+                              }
+                              else if (snapshot.hasError) {
+                                return Text('Error: ${snapshot.error}');
+                              }
+                              else if (snapshot.hasData) {
+                                List<String> tags = snapshot.data!;
+
+                                return Wrap(
+                                  spacing: 15,
+                                  runSpacing: 10,
+                                  alignment: WrapAlignment.start,
+                                  children: tags.map((tag) => TagBox(tag: tag)).toList(),
+                                );
+                              }
+                              return const Text('No tags available');
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 25,),
+                    Container(
+                      height: 1,
+                      color: primaryGrey,
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
+            Positioned(
+              bottom: 40,
+              left: 0,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // check major 버튼
+                  GestureDetector(
+                    onTap: () {
+                      Get.toNamed('/check_major');
+                    },
+                    child: BodyText(text: 'Edit major', color: Colors.black45, underline: true,),
+                  ),
+                  Container(width: 1, height: 15,color: primaryGrey, margin: const EdgeInsets.symmetric(horizontal: 7)),
+                  GestureDetector(
+                    onTap: () {
+                      Get.toNamed('/check_interest');
+                    },
+                    child: BodyText(text: 'Edit interest', color: Colors.black45, underline: true,),
+                  ),
+                  Container(width: 1, height: 15, color: primaryGrey, margin: const EdgeInsets.symmetric(horizontal: 7),),
+                  GestureDetector(
+                    onTap: () {
+                      _logout();
+                    },
+                    child: const Row(
+                      children: [
+                        BodyText(text: 'Logout', color: Colors.black45, underline: true,),
+                        SizedBox(width: 1,),
+                        Icon(Icons.logout_rounded, color: Colors.black45, size: 16,)
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
@@ -151,7 +188,7 @@ class _ProfileDrawerState extends State<ProfileDrawer> {
           return TitleText(
             text: snapshot.data!,
             size: size,
-            weight: FontWeight.w300,
+            weight: FontWeight.w500,
             color: color,
           );
         }
